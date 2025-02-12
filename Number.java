caa52ecd2c500c468da81cc5e6476d226430666e
Adhithyarajmk <adhithyaraj25@gmail.com>
2025-02-12 10:50:05 +0530

    Add files via upload

---
+++ Number.java
@@ -0,0 +1,19 @@
+public class Number
+{
+public static void main(String[]args)
+{
+int a=-4;
+if(a>0)
+{
+System.out.println("it is a positive number");
+}
+else if(a==0)
+{
+System.out.println("it is zero");
+}
+else
+{
+System.out.println("it is a negative number");
+}
+}
+}
